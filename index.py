1b228d6285d9c5753b7b04f6a4d554866805d234
Illiyyin Akbar Ariyanto <58885451+IlliyyinAkbar2002@users.noreply.github.com>
2023-01-07 23:01:55 +0700

    Create file index.py
    Simple project generator band name.

---
+++ index.py
@@ -0,0 +1,7 @@
+# Easy Way
+# Another solve problem create project Generator band name
+
+print("Welcome generator band name")
+city = input("What's the name of the city you grew up in?\n")
+name = input("What's your pet name?\n")
+print("Your band could be " + city + " " + name)
