7c618da148401b8752c5ac285af75b35f2cd5c5f
Illiyyin Akbar Ariyanto <58885451+IlliyyinAkbar2002@users.noreply.github.com>
2023-01-07 17:33:46 +0700

    Create file main.py
    Learn and create build program generator name

---
+++ main.py
@@ -0,0 +1,7 @@
+city = ""
+name = ""
+
+print("Welcome generator band name")
+print("What's the name of the city you grew up in?\n" + input(city))
+print("What's your pet name?\n" + input(name))
+print("Your band could be " + input(city) + " " + input(name))
